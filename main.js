0e41a924f1e38fc151956c46ef24fb67b8002086
NHX87 <nickharris1910@gmail.com>
2026-03-05 09:02:52 -0500

    redesign: dark/bold aesthetic with Space Mono labels, remove ambient blobs

--- main.js
+++ main.js
@@ -8,16 +8,3 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     }
   });
 });
-
-// Subtle parallax on ambient blobs
-const blobA = document.querySelector(".ambient-a");
-const blobB = document.querySelector(".ambient-b");
-
-if (blobA && blobB) {
-  window.addEventListener("mousemove", (e) => {
-    const x = e.clientX / window.innerWidth - 0.5;
-    const y = e.clientY / window.innerHeight - 0.5;
-    blobA.style.transform = `translate(${x * 18}px, ${y * 14}px)`;
-    blobB.style.transform = `translate(${x * -14}px, ${y * 10}px)`;
-  });
-}
